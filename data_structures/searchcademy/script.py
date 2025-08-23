5ec4da9a5574d693476b6babf4c171f1cd2b901c
j s <jlssz@proton.me>
2025-08-22 22:40:33 -0700

    updated script.py

--- data_structures/searchcademy/script.py
+++ data_structures/searchcademy/script.py
@@ -1,4 +1,42 @@
 def sparse_search(data, search_val):
-  print("Data: " + str(data))
-  print("Search Value: " + str(search_val))
-  
+    print("Data: " + str(data))
+    print("Search Value: " + str(search_val))
+    
+    first = data[0]
+    last = data[-1]
+
+    while first < last:
+        mid = (first + last) // 2
+
+        if not data[mid]:
+            left = mid - 1
+            right = mid + 1
+
+            while (not left) and (not right):
+                if left or right:
+                    break
+
+                if less < first and right > last:
+                    print("{0} is not in the dataset".format(search_val))
+                    return
+                elif right <= last and data[right]:
+                    mid = right
+                    break
+                elif left >= first and data[left]:
+                    mid = left
+                    break
+                else:
+                    right += 1
+                    left -= 1
+            
+        elif data[mid] == search_val:
+            print("{0} found at position {1}".format(search_val, mid))
+            return
+
+        elif data[mid] > search_val:
+            last = mid - 1
+        
+        elif data[mid] < search_val:
+            first = mid + 1
+
+    print("{0} is not in the dataset".format(search_val))    
