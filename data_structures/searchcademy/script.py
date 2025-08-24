3baca36af72ca22a92eba0d211a9716740637fd3
j s <jlssz@proton.me>
2025-08-23 21:19:18 -0700

    Fixed endless loop bug in inner while loop

--- data_structures/searchcademy/script.py
+++ data_structures/searchcademy/script.py
@@ -2,21 +2,18 @@ def sparse_search(data, search_val):
     print("Data: " + str(data))
     print("Search Value: " + str(search_val))
     
-    first = data[0]
-    last = data[-1]
+    first = 0
+    last = len(data) - 1
 
-    while first < last:
+    while first <= last:
         mid = (first + last) // 2
 
         if not data[mid]:
             left = mid - 1
             right = mid + 1
 
-            while (not left) and (not right):
-                if left or right:
-                    break
-
-                if less < first and right > last:
+            while True:
+                if left < first and right > last:
                     print("{0} is not in the dataset".format(search_val))
                     return
                 elif right <= last and data[right]:
@@ -25,11 +22,10 @@ def sparse_search(data, search_val):
                 elif left >= first and data[left]:
                     mid = left
                     break
-                else:
-                    right += 1
-                    left -= 1
+                right += 1
+                left -= 1
             
-        elif data[mid] == search_val:
+        if data[mid] == search_val:
             print("{0} found at position {1}".format(search_val, mid))
             return
 
